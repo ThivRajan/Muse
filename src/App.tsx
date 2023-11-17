f21c6f69db6b5899e10bef7d3ff7eac967f66f3b
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2023-11-17 16:02:51 -0500

    Setup project (#1)
    * Setup Vite, React & TypeScript
    
    * Setup ESLint rules
    
    * Setup TailwindCSS
    
    * Update dev server port
    
    * Create manifest to configure chrome extension
    
    * Add basic UI and extension script
    
    * Create script to build app for development

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+export default function App() {
+  const readDOM = async () => {
+    const [tab] = await chrome.tabs.query({
+      currentWindow: true,
+      active: true,
+    });
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id! },
+      func: () => {
+        alert("button clicked");
+      },
+    });
+  };
+
+  return (
+    <div className="w-[300px] p-8">
+      <h1 className="text-3xl font-bold text-blue-700">Hello world!</h1>
+      <button
+        className="text-xl bg-blue-500 text-white p-2 rounded hover:brightness-75"
+        onClick={readDOM}
+      >
+        Read DOM Content
+      </button>
+    </div>
+  );
+}
