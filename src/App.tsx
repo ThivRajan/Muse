9105324f7a83396f26280548a20ac8dc1ef93d4f
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2023-11-18 15:31:38 -0500

    Parse job postings (#2)
    * Add initial DOM parsing to extract text content
    
    * Move job parsing to util and read jobs off indeed
    
    * Add parsing for LinkedIn
    
    * Rename methods and util file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
+import { parseJob } from "./parse-job.util";
+
 export default function App() {
-  const readDOM = async () => {
+  const handleParsing = async () => {
     const [tab] = await chrome.tabs.query({
       currentWindow: true,
       active: true,
     });
     chrome.scripting.executeScript({
       target: { tabId: tab.id! },
-      func: () => {
-        alert("button clicked");
-      },
+      func: parseJob,
     });
   };
 
@@ -17,9 +17,9 @@ export default function App() {
       <h1 className="text-3xl font-bold text-blue-700">Hello world!</h1>
       <button
         className="text-xl bg-blue-500 text-white p-2 rounded hover:brightness-75"
-        onClick={readDOM}
+        onClick={handleParsing}
       >
-        Read DOM Content
+        Parse Job Text
       </button>
     </div>
   );
---
+++ src/parse-job.util.ts
@@ -0,0 +1,35 @@
+export function parseJob() {
+  const getJobPostingClass = () => {
+    const url = window.location.toString();
+
+    if (url.startsWith("https://ca.indeed.com/")) {
+      return "fastviewjob jobsearch-ViewJobLayout--embedded css-1lo7kga eu4oa1w0";
+    } else if (url.startsWith("https://www.linkedin.com")) {
+      return "job-view-layout jobs-details";
+    } else {
+      return "";
+    }
+  };
+
+  const jobContent = document
+    .getElementsByClassName(getJobPostingClass())
+    .item(0);
+
+  const getTextContent = (element: HTMLElement) => {
+    const range = document.createRange();
+    range.selectNode(element);
+
+    const selection = window.getSelection();
+    selection?.removeAllRanges();
+    selection?.addRange(range);
+
+    const textContent = selection?.toString();
+    selection?.removeAllRanges();
+
+    return textContent;
+  };
+
+  const jobText = jobContent ? getTextContent(jobContent as HTMLElement) : "";
+
+  alert(jobText);
+}
