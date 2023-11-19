b7d95d61746cab3270cdd24ed6317b895e2fc3e3
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2023-11-19 16:29:44 -0500

    Add sample resume for parsing and clean pdf parsing

--- src/utils/parse-pdf.util.ts
+++ src/utils/parse-pdf.util.ts
@@ -1,19 +1,19 @@
 import * as pdfjs from "pdfjs-dist";
 import pdfJSWorkerURL from "pdfjs-dist/build/pdf.worker?url";
 import { TextItem } from "pdfjs-dist/types/src/display/api";
+import sampleResume from "../assets/Sample Resume.pdf";
 
 export async function parsePdf() {
   pdfjs.GlobalWorkerOptions.workerSrc = pdfJSWorkerURL;
-  const pdfLoading = await pdfjs.getDocument("").promise;
+  const pdfLoading = await pdfjs.getDocument(sampleResume).promise;
   const resumePage = await pdfLoading.getPage(1);
   const textContent = await resumePage.getTextContent();
 
   const textItems = textContent.items;
-  let extractedText = "";
-
-  for (let i = 0; i < textItems.length; i++) {
-    extractedText += (textItems[i] as TextItem).str + " ";
-  }
+  const extractedText = textItems.reduce(
+    (acc: string, curr) => acc + (curr as TextItem).str + " ",
+    ""
+  );
 
   alert(extractedText);
 }
