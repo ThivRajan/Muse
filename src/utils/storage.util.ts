a874c2348e201d62c1b8bf65615ba112a2b3e970
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2023-11-21 16:15:58 -0500

    Move storage methods to util file

---
+++ src/utils/storage.util.ts
@@ -0,0 +1,86 @@
+import { parsePdf } from "./parse-pdf.util";
+
+const CHUNK_SIZE = 1024 * 100;
+
+function setChunkInStorage(key: string, chunk: ArrayBuffer) {
+  return new Promise<void>((resolve) => {
+    const uintArray = new Uint8Array(chunk);
+
+    // Convert Uint8Array to base64 string
+    let binary = "";
+    uintArray.forEach((byte) => {
+      binary += String.fromCharCode(byte);
+    });
+    let base64String = btoa(binary);
+
+    // Store the base64 string in Chrome storage
+    chrome.storage.local.set({ [key]: base64String }, () => {
+      resolve();
+    });
+  });
+}
+
+function getAllChunksFromStorage() {
+  return new Promise<{ [key: string]: any }>((resolve) => {
+    chrome.storage.local.get(null, (items) => {
+      resolve(items);
+    });
+  });
+}
+
+export async function loadFile(fileContents: ArrayBuffer) {
+  chrome.storage.local.clear();
+  // Split the array buffer into chunks
+  const chunks: ArrayBuffer[] = [];
+  for (let i = 0; i < fileContents.byteLength; i += CHUNK_SIZE) {
+    const chunk = fileContents.slice(i, i + CHUNK_SIZE);
+    chunks.push(chunk);
+  }
+
+  // Store the chunks in Chrome storage
+  for (let j = 0; j < chunks.length; j++) {
+    await setChunkInStorage(`resumeData_${j}`, chunks[j]);
+  }
+}
+
+export async function parseResume() {
+  const items = await getAllChunksFromStorage();
+
+  // Find all keys that contain chunks of the file
+  const chunkKeys = Object.keys(items).filter((key) =>
+    key.startsWith("resumeData_")
+  );
+
+  // Sort chunk keys based on the index (resumeData_0, resumeData_1, ...)
+  chunkKeys.sort((a, b) => {
+    const indexA = parseInt(a.split("_")[1]);
+    const indexB = parseInt(b.split("_")[1]);
+    return indexA - indexB;
+  });
+
+  // Create an ArrayBuffer to hold the reconstructed file
+  const totalSize = chunkKeys.length * CHUNK_SIZE;
+  const reconstructedArrayBuffer = new ArrayBuffer(totalSize);
+  const uint8Array = new Uint8Array(reconstructedArrayBuffer);
+
+  // Reconstruct the file from chunks
+  let offset = 0;
+  for (const key of chunkKeys) {
+    const base64String = items[key];
+    const binaryString = atob(base64String);
+
+    // Convert base64 string to Uint8Array
+    const chunkUint8Array = new Uint8Array(binaryString.length);
+    for (let i = 0; i < binaryString.length; i++) {
+      chunkUint8Array[i] = binaryString.charCodeAt(i);
+    }
+
+    // Copy chunk data to the reconstructed ArrayBuffer
+    uint8Array.set(chunkUint8Array, offset);
+    offset += CHUNK_SIZE;
+  }
+
+  const resumeFile = reconstructedArrayBuffer;
+
+  parsePdf(resumeFile);
+}
