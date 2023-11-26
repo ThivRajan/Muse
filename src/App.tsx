12011b7008d4afc48fb26d33bfed0c56a0aa439b
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2023-11-26 12:00:52 -0500

    Remove unused buttons from UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
 import { useState } from "react";
 import { displayCoverLetter } from "./utils/generate-cover-letter.util";
-import { parseJob } from "./utils/parse-job.util";
-import { parseResume, saveFile } from "./utils/storage.util";
+import { saveFile } from "./utils/storage.util";
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(false);
-
-  const handleJobParsing = async () => {
-    const [tab] = await chrome.tabs.query({
-      currentWindow: true,
-      active: true,
-    });
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id! },
-      func: parseJob,
-    });
-  };
 
   const saveResume = async () => {
     const resumeInput = document.getElementById(
@@ -33,13 +21,7 @@ export default function App() {
 
   return (
     <div className="w-[300px] p-8 flex flex-col gap-2">
-      <h1 className="text-3xl font-bold text-black">Hello world!</h1>
-      <button
-        className="text-xl bg-yellow-500 text-white p-2 rounded hover:brightness-75"
-        onClick={handleJobParsing}
-      >
-        Parse Job Text
-      </button>
+      <h1 className="text-3xl font-bold text-black">Muses</h1>
       <input type="file" id="resumeInput" />
       <button
         className="text-xl bg-green-500 text-white p-2 rounded hover:brightness-75"
@@ -49,12 +31,6 @@ export default function App() {
       </button>
       <button
         className="text-xl bg-blue-500 text-white p-2 rounded hover:brightness-75"
-        onClick={parseResume}
-      >
-        Parse Resume
-      </button>
-      <button
-        className="text-xl bg-gray-500 text-white p-2 rounded hover:brightness-75"
         onClick={() => displayCoverLetter(setIsLoading)}
       >
         Show Cover Letter
