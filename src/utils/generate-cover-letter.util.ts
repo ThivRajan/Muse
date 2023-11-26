5c756de188702e8bed65f881b9a2ea1683ebfb8a
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2023-11-26 15:29:47 -0500

    Update prompt to be more precise

--- src/utils/generate-cover-letter.util.ts
+++ src/utils/generate-cover-letter.util.ts
@@ -31,10 +31,11 @@ export async function generateCoverLetter(
 function getPrompt(jobDescription: string, resume: string) {
   return (
     `Please write a cover letter for the following job description and resume. ` +
-    `At the top of the cover letter, ` +
-    `add my phone number, name and email (you should have it from my resume) and today's date.` +
     `Here is the job description: \n\n${jobDescription}` +
     `\n\nHere is the resume: \n\n${resume}` +
-    `Please keep it within 250 words.`
+    `\n\nPlease keep it within 250 words. At the top of the cover letter, ` +
+    `add ONLY my phone number, name, email (you should have it from my resume) ` +
+    `and the text '[Date]' (without quotes), in that EXACT order and each on a separate line. ` +
+    `Do not include anything outside of than what I asked.`
   );
 }
