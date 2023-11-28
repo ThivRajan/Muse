91deba678a0f7ed19336ef6fe3141bcf377e50ea
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2023-11-28 14:08:38 -0500

    Optimize cover letter prompt (#7)
    * Add temperature in request to reduce randomness
    
    * Update prompt to be more precise
    
    * Replace date placeholder with today's date
    
    * Format date in cover letter
    
    * Move configurable properties into variables
    
    * Update header items and simplify date placeholder
    
    * Move formatting prompt into a separate variable
    
    * Move content prompt into a separate variable
    
    * Make prompt more concise

--- src/utils/generate-cover-letter.util.ts
+++ src/utils/generate-cover-letter.util.ts
@@ -1,5 +1,7 @@
 import { Dispatch, SetStateAction } from "react";
 
+const DATE_TOKEN = "[Date]";
+
 export async function generateCoverLetter(
   jobDescription: string,
   resume: string,
@@ -7,6 +9,8 @@ export async function generateCoverLetter(
 ) {
   const GPT_COMPLETION_ENDPOINT = "https://api.openai.com/v1/chat/completions";
   const API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
+
+  console.log({ prompt: getPrompt(jobDescription, resume) });
 
   setIsLoading(true);
   const response = await fetch(GPT_COMPLETION_ENDPOINT, {
@@ -18,22 +22,46 @@ export async function generateCoverLetter(
     body: JSON.stringify({
       model: "gpt-3.5-turbo-1106",
       messages: [{ role: "user", content: getPrompt(jobDescription, resume) }],
+      temperature: 0.5,
     }),
   });
   setIsLoading(false);
 
   const responseJSON = await response.json();
-  const coverLetter: string = responseJSON.choices[0].message.content;
+  const coverLetter: string = responseJSON.choices[0].message.content.replace(
+    DATE_TOKEN,
+    getFormattedDate()
+  );
   return coverLetter;
 }
 
 function getPrompt(jobDescription: string, resume: string) {
+  const headerItems = ["name", "my location", "email", "phone number"].join(
+    ", "
+  );
+  const wordCount = "200";
+
+  const contentPrompt =
+    `Please craft a cover letter within ${wordCount} words using the provided and job description. ` +
+    `Ensure the cover letter relates the resume's experience ` +
+    `to demonstrate how it aligns with the job description, showcasing value.`;
+
+  const formattingPrompt =
+    `Include ONLY the following details at the top of the cover letter in this EXACT order, each on a separate line:` +
+    `${headerItems}, and ${DATE_TOKEN}.`;
+
   return (
-    `Please write a cover letter for the following job description and resume. ` +
-    `At the top of the cover letter, ` +
-    `add my phone number, name and email (you should have it from my resume) and today's date.` +
+    `I am going to send you my resume along with a job description. ` +
     `Here is the job description: \n\n${jobDescription}` +
     `\n\nHere is the resume: \n\n${resume}` +
-    `Please keep it within 250 words.`
+    `\n\n${contentPrompt} ${formattingPrompt}`
   );
+}
+
+function getFormattedDate() {
+  return new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }).format(new Date());
 }
