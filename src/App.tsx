9e924265c757abb860ca649596fc1a85c27eea22
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2023-12-02 17:55:37 -0500

    Simplify UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { downloadCoverLetter } from "./utils/download-cover-letter.util";
 import { saveResumeToStorage } from "./utils/resume-storage.util";
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [resumeFileName, setResumeFileName] = useState("");
+  const resumeInputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     getResumeFileName();
   }, []);
+
+  const clearResumeFile = () => {
+    // TODO: Find a way to clear only local storage related to app
+    chrome.storage.local.clear();
+    setResumeFileName("");
+  };
 
   const getResumeFileName = async () => {
     const fileName = await chrome.storage.local.get("resumeFileName");
@@ -17,11 +24,8 @@ export default function App() {
     }
   };
 
-  const updateResume = async () => {
-    const resumeInput = document.getElementById(
-      "resumeInput"
-    ) as HTMLInputElement;
-    const resumeFile = resumeInput.files?.[0];
+  const handleResumeChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    const resumeFile = event.target.files?.[0];
     const resumeFileName = resumeFile?.name || "";
     const resumeFileContents = await resumeFile?.arrayBuffer();
 
@@ -34,26 +38,34 @@ export default function App() {
   };
 
   return (
-    <div className="w-[300px] p-8 flex flex-col gap-6">
+    <div className="w-[300px] p-8 flex flex-col gap-2">
       <h1 className="text-3xl font-bold text-black">Muses</h1>
-      <div className="flex flex-col gap-2">
-        <input type="file" id="resumeInput" className="hidden" />
-        <label htmlFor="resumeInput" className="flex gap-2 items-center">
-          <span className="bg-red-300 rounded w-fit p-1 text-white text-md cursor-pointer">
-            Choose Resume
+      <div>
+        <input
+          type="file"
+          id="resumeInput"
+          ref={resumeInputRef}
+          className="hidden"
+          onChange={handleResumeChange}
+        />
+        <div className="flex items-center gap-2">
+          <label htmlFor="resumeInput" className="flex gap-2 items-center">
+            <span className="bg-red-300 rounded w-fit p-1 text-white text-md cursor-pointer">
+              Choose Resume
+            </span>
+            {resumeFileName ? (
+              <span>{resumeFileName}</span>
+            ) : (
+              <span>No resume chosen</span>
+            )}
+          </label>
+          <span
+            className="hover:text-red-500 cursor-pointer text-lg"
+            onClick={clearResumeFile}
+          >
+            X
           </span>
-          {resumeFileName ? (
-            <span>{resumeFileName}</span>
-          ) : (
-            <span>No resume chosen</span>
-          )}
-        </label>
-        <button
-          className="text-xl bg-green-500 text-white p-2 rounded hover:brightness-75"
-          onClick={updateResume}
-        >
-          Update Resume
-        </button>
+        </div>
       </div>
       <button
         className="text-xl bg-blue-500 text-white p-2 rounded hover:brightness-75"
