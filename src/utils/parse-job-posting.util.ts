28ee14b1bcb53f5e247bcaf2d928fd26393d2b40
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2023-12-02 15:45:34 -0500

    Add job posting parsing for more job boards (#9)
    * Extract and use hostname to match job posting classes
    
    * Update getJobPostingClass to return multiple classes for different job posting formats
    
    * Add additional class for Indeed
    
    * Add classes for Glassdoor and other Indeed portals
    
    * Add a class for CareerBuilder

--- src/utils/parse-job-posting.util.ts
+++ src/utils/parse-job-posting.util.ts
@@ -1,19 +1,45 @@
 export async function parseJobPosting() {
   const getJobPostingClass = () => {
     const url = window.location.toString();
+    const hostname = url
+      .replace("http://", "")
+      .replace("https://", "")
+      .split("/")[0];
 
-    if (url.startsWith("https://ca.indeed.com/")) {
-      return "fastviewjob jobsearch-ViewJobLayout--embedded css-1lo7kga eu4oa1w0";
-    } else if (url.startsWith("https://www.linkedin.com")) {
-      return "job-view-layout jobs-details";
-    } else {
-      return "";
+    switch (hostname) {
+      case "ca.indeed.com":
+        return [
+          "fastviewjob jobsearch-ViewJobLayout--embedded css-1lo7kga eu4oa1w0",
+          "jobsearch-JobComponent css-u4y1in eu4oa1w0 jobsearch-JobComponent-bottomDivider",
+        ];
+      case "smartapply.indeed.com":
+      case "m5.apply.indeed.com":
+        return ["css-z7tuc2 e37uo190"];
+      case "www.linkedin.com":
+        return ["job-view-layout jobs-details"];
+      case "www.monster.ca":
+        return [
+          "jobview-containerstyles__JobViewCardWrapper-sc-16af7k7-0 kdCJsu",
+          "jobview-containerstyles__JobViewWrapper-sc-16af7k7-1 gfygNF",
+        ];
+      case "www.glassdoor.ca":
+        return ["JobDetails_jobDetailsContainer__sS1W1"];
+      case "www.careerbuilder.ca":
+        return ["data-display dn relative jdp-active"];
+      default:
+        return [];
     }
   };
 
-  const jobContent = document
-    .getElementsByClassName(getJobPostingClass())
-    .item(0);
+  const jobPostingClass = getJobPostingClass().find(
+    (postingClass) => document.getElementsByClassName(postingClass).length
+  );
+
+  if (!jobPostingClass) {
+    return "";
+  }
+
+  const jobContent = document.getElementsByClassName(jobPostingClass).item(0);
 
   const getTextContent = (element: HTMLElement) => {
     const range = document.createRange();
