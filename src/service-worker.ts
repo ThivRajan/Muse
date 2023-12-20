e2469d9f872a975c5637a344db2967d61b6c0a4e
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2023-12-20 16:00:18 -0500

    Create TS service worker

---
+++ src/service-worker.ts
@@ -0,0 +1,58 @@
+const JOB_BOARDS = [
+  "ca.indeed.com",
+  "smartapply.indeed.com",
+  "m5.apply.indeed.com",
+  "www.linkedin.com",
+  "www.monster.ca",
+  "www.glassdoor.ca",
+  "www.careerbuilder.ca",
+];
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+    if (tabs[0]?.id === tabId && changeInfo.status === "complete") {
+      await updateIcon(tab);
+    }
+  });
+});
+
+chrome.tabs.onActivated.addListener(() => {
+  chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+    if (tabs[0]) {
+      await updateIcon(tabs[0]);
+    }
+  });
+});
+
+chrome.runtime.onConnect.addListener((port) => {
+  port.onMessage.addListener(async ({ checkJobBoard }) => {
+    if (!checkJobBoard) {
+      return;
+    }
+
+    chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+      if (tabs[0]) {
+        const jobBoardFound = await isJobBoardFound(tabs[0]);
+        port.postMessage({ jobBoardFound });
+      } else {
+        port.postMessage({ jobBoardFound: false });
+      }
+    });
+  });
+});
+
+async function updateIcon(tab: chrome.tabs.Tab) {
+  const jobBoardFound = await isJobBoardFound(tab);
+  await chrome.action.setIcon({
+    tabId: tab.id,
+    path: jobBoardFound ? "logo-active-38.png" : "logo-inactive-38.png",
+  });
+}
+
+async function isJobBoardFound(tab: chrome.tabs.Tab) {
+  const hostname = (tab.url || "")
+    .replace("http://", "")
+    .replace("https://", "")
+    .split("/")[0];
+  return JOB_BOARDS.includes(hostname);
+}
